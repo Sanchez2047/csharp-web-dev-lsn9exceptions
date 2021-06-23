4d2ade029f2e021cf52d09b7da04378816100b3e
Mike <mike.sierra.7950@gmail.com>
2021-06-23 18:53:52 -0400

    THIS IS DONE

--- csharp-web-dev-lsn9exceptions/Program.cs
+++ csharp-web-dev-lsn9exceptions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 //Exercises for the Exceptions Chapter
 
@@ -7,22 +8,47 @@ namespace csharp_web_dev_lsn9exceptions
 {
     class Program
     {
-        /*
-        static double Divide(double x, double y)
+        static Double Divide (double x, double y)
         {
-            // Write your code here!
+            return x / y;
         }
-
-        static int CheckFileExtension(string fileName)
+        static int checkfileextension(string filename)
         {
-            // Write your code here!
-        }
-        */
+            if (filename.Contains(".cs"))
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
 
+        }
         static void Main(string[] args)
         {
-            // Test out your Divide() function here!
-
+            double num1 = 10;
+            double num2 = 0;
+            double solution;
+            
+                // Test out your Divide() function here!
+             
+            solution = Divide(num1, num2);
+            try
+        {
+            if (num2 != 0)
+            {
+                Console.WriteLine(solution);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Value cannot be divided by zero.");
+            }
+        }
+        catch (ArgumentOutOfRangeException e)
+        {
+            Console.WriteLine(e);
+            
+        }
             // Test out your CheckFileExtension() function here!
             Dictionary<string, string> students = new Dictionary<string, string>();
             students.Add("Carl", "Program.cs");
@@ -30,7 +56,27 @@ namespace csharp_web_dev_lsn9exceptions
             students.Add("Elizabeth", "MyCode.cs");
             students.Add("Stefanie", "CoolProgram.cs");
 
+            foreach(var student in students)
+            {
+                try
+                {
+                    if (student.Value.Equals(""))
+                    {
+                        throw new ArgumentNullException($"{student.Key} did not submit assignment");
+                    }
+                    else
+                    {
+                    int score = checkfileextension(student.Value);
+                    Console.WriteLine($"{student.Key} : {score}");
+                    }
+                }
+                catch (ArgumentNullException f)
+                {
+                    Console.WriteLine(f);
+                }
+            }
 
+            Console.ReadLine();
         }
     }
 }
